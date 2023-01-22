5f9d2110254ad29507e3d53f5c0567bf47dbb1f6
Justin Elliott <thecrashtestjustin@gmail.com>
2023-01-21 22:01:25 -0800

    updates to game functions

--- game.js
+++ game.js
@@ -26,31 +26,43 @@ const playerFactory = (name, role) => {
 
 //game functionality
 var gameController = (function () {
+  const startGameButton = document.querySelector(".start");
   var players = [];
-  var joe = playerFactory("Joe", "X");
-  var jane = playerFactory("Jane", "Y");
-  players.push(joe);
-  players.push(jane);
-  console.log(players);
-  joe.welcomePlayerX();
-  jane.welcomePlayerY();
-  return {
-    runPLayerMove: function () {
-      gameBoard(role);
-    },
-  };
+  startGameButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("test");
+    playerFactory(p1Name, p1RoleX);
+    playerFactory(p2Name, p2RoleY);
+  });
+  //   var joe = playerFactory("Joe", "X");
+  //   var jane = playerFactory("Jane", "Y");
+  //   players.push(joe);
+  //   players.push(jane);
+  //   console.log(players);
+  //   joe.welcomePlayerX();
+  //   jane.welcomePlayerY();
+
+  //   return {
+  //     runPLayerMove: function () {
+  //       gameBoard(role);
+  //     },
+  //   };
 })();
 
 //game controls setup
 var gameSetup = (function () {
   const pvc = document.getElementById("pvc");
   const pvp = document.getElementById("pvp");
+  const p1Name = document.getElementById("p1-name");
   const p2Name = document.getElementById("p2-name");
   const p2Label = document.querySelector(".p2-name-label");
   const p2RoleTitle = document.querySelector(".p2-choose-role");
+  const p1RoleX = document.getElementById("p1-role-x");
   const p2RoleX = document.getElementById("p2-role-x");
+  const p1RoleY = document.getElementById("p1-role-y");
   const p2RoleY = document.getElementById("p2-role-y");
   const p2RoleLabel = document.querySelectorAll(".p2-role-label");
+  const inputs = document.querySelectorAll("input");
   pvc.addEventListener("click", (e) => {
     p2Name.disabled = true;
     p2RoleX.disabled = true;
