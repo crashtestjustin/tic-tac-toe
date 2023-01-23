b06f7edb476abf772530a9e799b97d1f6f2d7516
Justin Elliott <thecrashtestjustin@gmail.com>
2023-01-23 08:07:59 -0800

    updates

--- game.js
+++ game.js
@@ -1,46 +1,76 @@
 //game board
 var gameBoard = (function (move) {
-  const board = document.querySelector(".board");
-  board.addEventListener("click", (e) => {
-    const squares = e.target.closest(".board-square");
-    if (squares.innerHTML !== "") {
-      return;
-    } else {
-      squares.innerHTML = "X";
-    }
+  const boardSq = document.querySelectorAll(".board-square");
+  boardSq.forEach((sq) => {
+    sq.addEventListener("click", (e) => {
+      const squares = e.target.closest(".board-square");
+      if (squares.innerHTML !== "") {
+        return;
+      } else {
+        squares.innerHTML = "X";
+      }
+    });
   });
 })();
 
 //player creation
-const playerFactory = (name, role) => {
-  const welcomePlayerX = () =>
-    console.log(
-      `Welcome to the game ${name}. You chose to be player ${role}. You get to go first.`
-    );
-  const welcomePlayerY = () =>
-    console.log(
-      `Welcome to the game ${name}. You chose to be player ${role}. You will go second.`
-    );
-  return { name, role, welcomePlayerX, welcomePlayerY };
-};
+function personFactory(name, role) {
+  return {
+    name: name,
+    role: role,
+    welcomeMessage() {
+      if (role === "X") {
+        console.log(
+          `Welcome ${name}. You chose to be player ${role}, you will go first.`
+        );
+      } else {
+        console.log(
+          `Welcome ${name}. You chose to be player ${role}, you will go second.`
+        );
+      }
+    },
+  };
+}
 
 //game functionality
-var gameController = (function () {})();
+var gameController = (function () {
+  const startGameButton = document.querySelector(".start");
+  const p1Name = document.getElementById("p1-name");
+  const p2Name = document.getElementById("p2-name");
+  const p1RoleX = document.getElementById("p1-role-x").value;
+  const p2RoleX = document.getElementById("p2-role-x").value;
+  const p1RoleY = document.getElementById("p1-role-y").value;
+  const p2RoleY = document.getElementById("p2-role-y").value;
+  var playerX;
+  var playerO;
+  startGameButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (p1RoleX.checked) {
+      playerX = personFactory(p1Name.value, p1RoleX);
+      playerO = personFactory(p2Name.value, p2RoleY);
+      console.log(playerX, playerO);
+      playerX.welcomeMessage();
+      playerO.welcomeMessage();
+    } else {
+      playerO = personFactory(p1Name.value, p1RoleY);
+      playerX = personFactory(p2Name.value, p2RoleX);
+      console.log(playerX, playerO);
+      playerX.welcomeMessage();
+      playerO.welcomeMessage();
+    }
+  });
+})();
 
 //game controls setup
 var gameSetup = (function () {
   const pvc = document.getElementById("pvc");
   const pvp = document.getElementById("pvp");
-  const p1Name = document.getElementById("p1-name");
   const p2Name = document.getElementById("p2-name");
   const p2Label = document.querySelector(".p2-name-label");
   const p2RoleTitle = document.querySelector(".p2-choose-role");
-  const p1RoleX = document.getElementById("p1-role-x");
   const p2RoleX = document.getElementById("p2-role-x");
-  const p1RoleY = document.getElementById("p1-role-y");
   const p2RoleY = document.getElementById("p2-role-y");
   const p2RoleLabel = document.querySelectorAll(".p2-role-label");
-  const inputs = document.querySelectorAll("input");
   pvc.addEventListener("click", (e) => {
     p2Name.disabled = true;
     p2RoleX.disabled = true;
@@ -63,33 +93,3 @@ var gameSetup = (function () {
     });
   });
 })();
-
-//previous code - don't come back to this unless absolutely necessary
-// const startGameButton = document.querySelector(".start");
-// const p1Name = document.getElementById("p1-name");
-// const p2Name = document.getElementById("p2-name");
-// const p1RoleX = document.getElementById("p1-role-x");
-// const p2RoleX = document.getElementById("p2-role-x");
-// const p1RoleY = document.getElementById("p1-role-y");
-// const p2RoleY = document.getElementById("p2-role-y");
-// startGameButton.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   console.log("test");
-//   var playerOne = playerFactory(p1Name.value, p1RoleX.value);
-//   var playerTwo = playerFactory(p2Name.value, p2RoleY.value);
-//   playerOne.welcomePlayerX();
-//   playerTwo.welcomePlayerY();
-// });
-//   var joe = playerFactory("Joe", "X");
-//   var jane = playerFactory("Jane", "Y");
-//   players.push(joe);
-//   players.push(jane);
-//   console.log(players);
-//   joe.welcomePlayerX();
-//   jane.welcomePlayerY();
-
-//   return {
-//     runPLayerMove: function () {
-//       gameBoard(role);
-//     },
-//   };
--- readme.md
+++ readme.md
@@ -1,8 +1,1 @@
-Changes to do:
 
-- Move the didsplay funtion into the game controller module
-- create listeners fro placing new moves on the game grid
-- prevent adding moves to existing squares that have moves
-- add logic to check for a win or tie
-- add UI for players to inpour their name and select the role they want to play
-- create a computer AI that plays the opposite role
